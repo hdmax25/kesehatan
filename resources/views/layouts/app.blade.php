53806194ef37e96c9b442928c83aaadde5538043
Nier <com.owl.minerva@gmail.com>
2020-05-31 19:05:00 +0700

    X

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>MTS</title>
+  <title>IMS</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Favicon icon -->
   <link rel="shortcut icon" href="{{ asset('img/logo.png') }}" type="image/x-icon">
