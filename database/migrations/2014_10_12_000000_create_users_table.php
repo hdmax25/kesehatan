006be5d031d9d9d34460e706148adf6b9500fbc5
hdmax25 <47875083+hdmax25@users.noreply.github.com>
2020-05-31 17:52:02 +0700

    x

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,17 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('nip')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role');
+            $table->integer('id_departement');
+            $table->string('phone');
+            $table->string('position');
+            $table->string('ktpaddress');
+            $table->string('domicile');
+            $table->integer('id_penyakit');
+            $table->string('complaint');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
