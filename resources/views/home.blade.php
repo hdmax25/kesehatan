77d3c17fa7e84fb8f75ca1a892318df322a9b601
Nier <com.owl.minerva@gmail.com>
2020-05-31 18:43:40 +0700

    x

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
+@section('title')
+  <div class="row mb-2">
+    <div class="col-sm-6">
+      <h1>
+        Home
+      </h1>
+    </div>
+    <div class="col-sm-6">
+      <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item">
+          <a href="#">
+            Home
+          </a>
+        </li>
+      </ol>
+    </div>
+  </div>
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
