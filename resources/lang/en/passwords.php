9a79e3d5aa2ca47f22963a4d7f9d5b4d422e98d5
Nier <com.owl.minerva@gmail.com>
2020-06-01 00:17:25 +0700

    x

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -1,22 +1,9 @@
+
 <?php
-
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
-
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
-
+    'password' => 'Kata sandi harus minimal enam karakter dan cocok dengan konfirmasi.',
+    'reset'    => 'Kata sandi Anda sudah di atur ulang!',
+    'sent'     => 'Kami sudah mengirim email yang berisi tautan untuk mengatur ulang kata sandi Anda!',
+    'token'    => 'Kata sandi token pengaturan ulang tidak sah.',
+    'user'     => 'Kami tidak dapat menemukan pengguna dengan alamat surel tersebut.',
 ];
--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -1,151 +1,70 @@
 <?php
-
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Validation Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines contain the default error messages used by
-    | the validator class. Some of these rules have multiple versions such
-    | as the size rules. Feel free to tweak each of these messages here.
-    |
-    */
-
-    'accepted' => 'The :attribute must be accepted.',
-    'active_url' => 'The :attribute is not a valid URL.',
-    'after' => 'The :attribute must be a date after :date.',
-    'after_or_equal' => 'The :attribute must be a date after or equal to :date.',
-    'alpha' => 'The :attribute may only contain letters.',
-    'alpha_dash' => 'The :attribute may only contain letters, numbers, dashes and underscores.',
-    'alpha_num' => 'The :attribute may only contain letters and numbers.',
-    'array' => 'The :attribute must be an array.',
-    'before' => 'The :attribute must be a date before :date.',
-    'before_or_equal' => 'The :attribute must be a date before or equal to :date.',
-    'between' => [
-        'numeric' => 'The :attribute must be between :min and :max.',
-        'file' => 'The :attribute must be between :min and :max kilobytes.',
-        'string' => 'The :attribute must be between :min and :max characters.',
-        'array' => 'The :attribute must have between :min and :max items.',
+    'accepted'             => 'Isian :attribute harus diterima.',
+    'active_url'           => 'Isian :attribute bukan URL yang sah.',
+    'after'                => 'Isian :attribute harus tanggal setelah :date.',
+    'alpha'                => 'Isian :attribute hanya boleh berisi huruf.',
+    'alpha_dash'           => 'Isian :attribute hanya boleh berisi huruf, angka, dan strip.',
+    'alpha_num'            => 'Isian :attribute hanya boleh berisi huruf dan angka.',
+    'array'                => 'Isian :attribute harus berupa sebuah array.',
+    'before'               => 'Isian :attribute harus tanggal sebelum :date.',
+    'between'              => [
+        'numeric' => 'Isian :attribute harus antara :min dan :max.',
+        'file'    => 'Isian :attribute harus antara :min dan :max kilobytes.',
+        'string'  => 'Isian :attribute harus antara :min dan :max karakter.',
+        'array'   => 'Isian :attribute harus antara :min dan :max item.',
     ],
-    'boolean' => 'The :attribute field must be true or false.',
-    'confirmed' => 'The :attribute confirmation does not match.',
-    'date' => 'The :attribute is not a valid date.',
-    'date_equals' => 'The :attribute must be a date equal to :date.',
-    'date_format' => 'The :attribute does not match the format :format.',
-    'different' => 'The :attribute and :other must be different.',
-    'digits' => 'The :attribute must be :digits digits.',
-    'digits_between' => 'The :attribute must be between :min and :max digits.',
-    'dimensions' => 'The :attribute has invalid image dimensions.',
-    'distinct' => 'The :attribute field has a duplicate value.',
-    'email' => 'The :attribute must be a valid email address.',
-    'ends_with' => 'The :attribute must end with one of the following: :values.',
-    'exists' => 'The selected :attribute is invalid.',
-    'file' => 'The :attribute must be a file.',
-    'filled' => 'The :attribute field must have a value.',
-    'gt' => [
-        'numeric' => 'The :attribute must be greater than :value.',
-        'file' => 'The :attribute must be greater than :value kilobytes.',
-        'string' => 'The :attribute must be greater than :value characters.',
-        'array' => 'The :attribute must have more than :value items.',
+    'boolean'              => 'Isian :attribute harus berupa true atau false',
+    'confirmed'            => 'Konfirmasi :attribute tidak cocok.',
+    'date'                 => 'Isian :attribute bukan tanggal yang valid.',
+    'date_format'          => 'Isian :attribute tidak cocok dengan format :format.',
+    'different'            => 'Isian :attribute dan :other harus berbeda.',
+    'digits'               => 'Isian :attribute harus berupa angka :digits.',
+    'digits_between'       => 'Isian :attribute harus antara angka :min dan :max.',
+    'dimensions'           => 'Isian :attribute harus merupakan dimensi gambar yang sah.',
+    'distinct'             => 'Isian :attribute memiliki nilai yang duplikat.',
+    'email'                => 'Isian :attribute harus berupa alamat surel yang valid.',
+    'exists'               => 'Isian :attribute yang dipilih tidak valid.',
+    'filled'               => 'Isian :attribute wajib diisi.',
+    'image'                => 'Isian :attribute harus berupa gambar.',
+    'in'                   => 'Isian :attribute yang dipilih tidak valid.',
+    'in_array'             => 'Isian :attribute tidak terdapat dalam :other.',
+    'integer'              => 'Isian :attribute harus merupakan bilangan bulat.',
+    'ip'                   => 'Isian :attribute harus berupa alamat IP yang valid.',
+    'json'                 => 'Isian :attribute harus berupa JSON string yang valid.',
+    'max'                  => [
+        'numeric' => 'Isian :attribute seharusnya tidak lebih dari :max.',
+        'file'    => 'Isian :attribute seharusnya tidak lebih dari :max kilobytes.',
+        'string'  => 'Isian :attribute seharusnya tidak lebih dari :max karakter.',
+        'array'   => 'Isian :attribute seharusnya tidak lebih dari :max item.',
     ],
-    'gte' => [
-        'numeric' => 'The :attribute must be greater than or equal :value.',
-        'file' => 'The :attribute must be greater than or equal :value kilobytes.',
-        'string' => 'The :attribute must be greater than or equal :value characters.',
-        'array' => 'The :attribute must have :value items or more.',
+    'mimes'                => 'Isian :attribute harus dokumen berjenis : :values.',
+    'min'                  => [
+        'numeric' => 'Isian :attribute harus minimal :min.',
+        'file'    => 'Isian :attribute harus minimal :min kilobytes.',
+        'string'  => 'Isian :attribute harus minimal :min karakter.',
+        'array'   => 'Isian :attribute harus minimal :min item.',
     ],
-    'image' => 'The :attribute must be an image.',
-    'in' => 'The selected :attribute is invalid.',
-    'in_array' => 'The :attribute field does not exist in :other.',
-    'integer' => 'The :attribute must be an integer.',
-    'ip' => 'The :attribute must be a valid IP address.',
-    'ipv4' => 'The :attribute must be a valid IPv4 address.',
-    'ipv6' => 'The :attribute must be a valid IPv6 address.',
-    'json' => 'The :attribute must be a valid JSON string.',
-    'lt' => [
-        'numeric' => 'The :attribute must be less than :value.',
-        'file' => 'The :attribute must be less than :value kilobytes.',
-        'string' => 'The :attribute must be less than :value characters.',
-        'array' => 'The :attribute must have less than :value items.',
+    'not_in'               => 'Isian :attribute yang dipilih tidak valid.',
+    'numeric'              => 'Isian :attribute harus berupa angka.',
+    'present'              => 'Isian :attribute wajib ada.',
+    'regex'                => 'Format isian :attribute tidak valid.',
+    'required'             => 'Isian :attribute wajib diisi.',
+    'required_if'          => 'Isian :attribute wajib diisi bila :other adalah :value.',
+    'required_unless'      => 'Isian :attribute wajib diisi kecuali :other memiliki nilai :values.',
+    'required_with'        => 'Isian :attribute wajib diisi bila terdapat :values.',
+    'required_with_all'    => 'Isian :attribute wajib diisi bila terdapat :values.',
+    'required_without'     => 'Isian :attribute wajib diisi bila tidak terdapat :values.',
+    'required_without_all' => 'Isian :attribute wajib diisi bila tidak terdapat ada :values.',
+    'same'                 => 'Isian :attribute dan :other harus sama.',
+    'size'                 => [
+        'numeric' => 'Isian :attribute harus berukuran :size.',
+        'file'    => 'Isian :attribute harus berukuran :size kilobyte.',
+        'string'  => 'Isian :attribute harus berukuran :size karakter.',
+        'array'   => 'Isian :attribute harus mengandung :size item.',
     ],
-    'lte' => [
-        'numeric' => 'The :attribute must be less than or equal :value.',
-        'file' => 'The :attribute must be less than or equal :value kilobytes.',
-        'string' => 'The :attribute must be less than or equal :value characters.',
-        'array' => 'The :attribute must not have more than :value items.',
-    ],
-    'max' => [
-        'numeric' => 'The :attribute may not be greater than :max.',
-        'file' => 'The :attribute may not be greater than :max kilobytes.',
-        'string' => 'The :attribute may not be greater than :max characters.',
-        'array' => 'The :attribute may not have more than :max items.',
-    ],
-    'mimes' => 'The :attribute must be a file of type: :values.',
-    'mimetypes' => 'The :attribute must be a file of type: :values.',
-    'min' => [
-        'numeric' => 'The :attribute must be at least :min.',
-        'file' => 'The :attribute must be at least :min kilobytes.',
-        'string' => 'The :attribute must be at least :min characters.',
-        'array' => 'The :attribute must have at least :min items.',
-    ],
-    'not_in' => 'The selected :attribute is invalid.',
-    'not_regex' => 'The :attribute format is invalid.',
-    'numeric' => 'The :attribute must be a number.',
-    'password' => 'The password is incorrect.',
-    'present' => 'The :attribute field must be present.',
-    'regex' => 'The :attribute format is invalid.',
-    'required' => 'The :attribute field is required.',
-    'required_if' => 'The :attribute field is required when :other is :value.',
-    'required_unless' => 'The :attribute field is required unless :other is in :values.',
-    'required_with' => 'The :attribute field is required when :values is present.',
-    'required_with_all' => 'The :attribute field is required when :values are present.',
-    'required_without' => 'The :attribute field is required when :values is not present.',
-    'required_without_all' => 'The :attribute field is required when none of :values are present.',
-    'same' => 'The :attribute and :other must match.',
-    'size' => [
-        'numeric' => 'The :attribute must be :size.',
-        'file' => 'The :attribute must be :size kilobytes.',
-        'string' => 'The :attribute must be :size characters.',
-        'array' => 'The :attribute must contain :size items.',
-    ],
-    'starts_with' => 'The :attribute must start with one of the following: :values.',
-    'string' => 'The :attribute must be a string.',
-    'timezone' => 'The :attribute must be a valid zone.',
-    'unique' => 'The :attribute has already been taken.',
-    'uploaded' => 'The :attribute failed to upload.',
-    'url' => 'The :attribute format is invalid.',
-    'uuid' => 'The :attribute must be a valid UUID.',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Custom Validation Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify custom validation messages for attributes using the
-    | convention "attribute.rule" to name the lines. This makes it quick to
-    | specify a specific custom language line for a given attribute rule.
-    |
-    */
-
-    'custom' => [
-        'attribute-name' => [
-            'rule-name' => 'custom-message',
-        ],
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Custom Validation Attributes
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used to swap our attribute placeholder
-    | with something more reader friendly such as "E-Mail Address" instead
-    | of "email". This simply helps us make our message more expressive.
-    |
-    */
-
-    'attributes' => [],
-
+    'string'               => 'Isian :attribute harus berupa string.',
+    'timezone'             => 'Isian :attribute harus berupa zona waktu yang valid.',
+    'unique'               => 'Isian :attribute sudah ada sebelumnya.',
+    'url'                  => 'Format isian :attribute tidak valid.',
 ];
