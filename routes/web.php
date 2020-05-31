a2a7e401d1d78a7005b6f80bde525d81af5ba2c4
Nier <com.owl.minerva@gmail.com>
2020-05-31 17:25:57 +0700

    setting roting and make user controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'user', 'as' => 'user.'], static function () {
+  Route::get('/', 'UserController@index')->name('index')->middleware('auth', 'role:1');
+  Route::get('/create', 'UserController@create')->name('create')->middleware('auth', 'role:1');
+  Route::post('/store', 'UserController@store')->name('store')->middleware('auth', 'role:1');
+  Route::get('/show/{id}', 'UserController@show')->name('show')->middleware('auth');
+  Route::get('/edit/{id}', 'UserController@edit')->name('edit')->middleware('auth', 'role:1');
+  Route::post('/update/{id}', 'UserController@update')->name('update')->middleware('auth', 'role:1');
+  Route::get('/destroy/{id}', 'UserController@destroy')->name('destroy')->middleware('auth', 'role:1');
+});
