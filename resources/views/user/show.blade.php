2078d755efdd1edbb21f27b1bdce3710a6b0dae5
Nier <com.owl.minerva@gmail.com>
2020-06-03 20:29:50 +0700

    Update show.blade.php

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -20,139 +20,115 @@
 @endsection
 
 @section('content')
-<div class="row">
-          <div class="col-md-3">
-
-            <!-- Profile Image -->
-            <div class="card card-primary card-outline">
-              <div class="card-body box-profile">
-                <div class="text-center">
-                  <img class="profile-user-img img-fluid img-circle" src="{{ asset('dist/img/avatar5.png') }}" alt="User profile picture">
-                </div>
-
-                <h3 class="profile-username text-center">Nama</h3>
-
-                <p class="text-muted text-center">Department</p>
-
-                <ul class="list-group list-group-unbordered mb-3">
-                  <li class="list-group-item">
-                    <b>NIP</b> <a class="float-right">6510977</a>
-                  </li>
-                  <li class="list-group-item">
-                    <b>Phone</b> <a class="float-right">08123456789</a>
-                  </li>
-                  <li class="list-group-item">
-                    <b>Alamat</b> <a class="float-right">ktpaddres</a>
-                  </li>
-                </ul>
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
+  <div class="row">
+    <div class="col-md-3">
+      <div class="card card-danger card-outline">
+        <div class="card-body box-profile">
+          <div class="text-center">
+            <img class="profile-user-img img-fluid img-circle" src="{{ asset('dist/img/avatar5.png') }}" alt="User profile picture">
           </div>
-          <!-- /.col -->
-          <div class="col-md-9">
-            <div class="card">
-              <div class="card-header p-2">
-                <ul class="nav nav-pills">
-                  <li class="nav-item"><a class="nav-link active" href="#timeline" data-toggle="tab">History</a></li>
-                  <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Edit</a></li>
-                </ul>
-              </div><!-- /.card-header -->
-              <div class="card-body">
-                <div class="tab-content">
-                  <div class="tab-pane active" id="timeline">
-                    <div class="timeline timeline-inverse">
-                      <!-- timeline time label -->
-                      <div class="time-label">
-                        <span class="bg-success">
-                          10 Februari 2020
-                        </span>
-                      </div>
-                      <!-- /.timeline-label -->
-                      <!-- timeline item -->
-                      <div>
-                        <i class="fas fa-heartbeat bg-success"></i>
-                        <div class="timeline-item">
-                          <span class="time"><i class="far fa-clock"></i> 12:05</span>
 
-                          <h3 class="timeline-header">Penyakit - di Posisi</h3>
+          <h3 class="profile-username text-center">{{ $user->name }}</h3>
 
-                          <div class="timeline-body">
-                            Keluhan
-                          </div>
-                          <div class="timeline-footer">
-                          </div>
-                        </div>
-                      </div>
-                      <!-- END timeline item -->
-                      <!-- timeline item -->
-                      <div>
-                        <i class="far fa-clock bg-gray"></i>
-                      </div>
+          <p class="text-muted text-center">{{ $user->department ? $user->department->name : '' }}</p>
+
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <div class="nav-link">
+                NIP <span class="float-right">{{ $user->username }}</span>
+              </div>
+            </li>
+            <li class="nav-item">
+              <div class="nav-link">
+                Phone <span class="float-right">{{ $user->phone }}</span>
+              </div>
+            </li>
+            <li class="nav-item">
+              <div class="nav-link">
+                Alamat <span class="float-right">{{ $user->ktpaddress }}</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-9">
+      <div class="card">
+        <div class="card-header p-2">
+          <ul class="nav nav-pills">
+            <li class="nav-item"><a class="nav-link active" href="#timeline" data-toggle="tab">History</a></li>
+            <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Edit</a></li>
+          </ul>
+        </div>
+        <div class="card-body">
+          <div class="tab-content">
+            <div class="tab-pane active" id="timeline">
+              <div class="timeline timeline-inverse">
+                <div class="time-label">
+                  <span class="bg-success">
+                    10 Februari 2020
+                  </span>
+                </div>
+                <div>
+                  <i class="fas fa-heartbeat bg-success"></i>
+                  <div class="timeline-item">
+                    <span class="time"><i class="far fa-clock"></i> 12:05</span>
+
+                    <h3 class="timeline-header">Penyakit - di Posisi</h3>
+
+                    <div class="timeline-body">
+                      Keluhan
+                    </div>
+                    <div class="timeline-footer">
                     </div>
                   </div>
-                  <!-- /.tab-pane -->
-
-                  <div class="tab-pane" id="settings">
-                    <form class="form-horizontal">
-                      <div class="form-group row">
-                        <label for="inputNIP" class="col-sm-2 col-form-label">NIP</label>
-                        <div class="col-sm-10">
-                          <input type="text" class="form-control" id="inputNIP" placeholder="Name">
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <label for="inputPassword" class="col-sm-2 col-form-label">Pasword</label>
-                        <div class="col-sm-10">
-                          <input type="password" class="form-control" id="Password" placeholder="Name">
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <label for="inputName" class="col-sm-2 col-form-label">Nama</label>
-                        <div class="col-sm-10">
-                          <input type="text" class="form-control" id="inputName" placeholder="Nama">
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <label for="inputName2" class="col-sm-2 col-form-label">Unit Kerja</label>
-                        <div class="col-sm-10">
-                            <select id="department" name="department" class="form-control select2-danger" data-dropdown-css-class="select2-danger" required>
-                            <option value="1">Departemen</option>
-                            </select>
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <label for="inputExperience" class="col-sm-2 col-form-label">Alamat</label>
-                        <div class="col-sm-10">
-                          <textarea class="form-control" id="inputExperience" placeholder="Alamat">KTP ADDRESS</textarea>
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <div class="offset-sm-2 col-sm-10">
-                          <div class="checkbox">
-                            <label>
-                              <input type="checkbox"> Data sudah benar
-                            </label>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <div class="offset-sm-2 col-sm-10">
-                          <button type="submit" class="btn btn-danger">Submit</button>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                  <!-- /.tab-pane -->
                 </div>
-                <!-- /.tab-content -->
-              </div><!-- /.card-body -->
+                <div>
+                  <i class="far fa-clock bg-gray"></i>
+                </div>
+              </div>
             </div>
-            <!-- /.nav-tabs-custom -->
+            <div class="tab-pane" id="settings">
+              <form class="form-horizontal">
+                <div class="form-group row">
+                  <label for="inputName" class="col-sm-2 col-form-label">Nama</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputName" placeholder="Nama">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="inputPassword" class="col-sm-2 col-form-label">Pasword</label>
+                  <div class="col-sm-10">
+                    <input type="password" class="form-control" id="Password" placeholder="Name">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="inputExperience" class="col-sm-2 col-form-label">Alamat</label>
+                  <div class="col-sm-10">
+                    <textarea class="form-control" id="inputExperience" placeholder="Alamat">KTP ADDRESS</textarea>
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <div class="offset-sm-2 col-sm-10">
+                    <div class="checkbox">
+                      <label>
+                        <input type="checkbox"> Data sudah benar
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <div class="offset-sm-2 col-sm-10">
+                    <button type="submit" class="btn btn-danger">Submit</button>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
-          <!-- /.col -->
         </div>
+      </div>
+    </div>
+  </div>
 @endsection
 
 @section('css')
