8b9872b0e8f8925df197222dda7261e5121e7c3a
hdmax25 <47875083+hdmax25@users.noreply.github.com>
2020-06-04 20:51:26 +0700

    Update show.blade.php

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -4,17 +4,8 @@
   <div class="row mb-2">
     <div class="col-sm-6">
       <h1>
-        Home
+        Profile
       </h1>
-    </div>
-    <div class="col-sm-6">
-      <ol class="breadcrumb float-sm-right">
-        <li class="breadcrumb-item">
-          <a href="#">
-            Home
-          </a>
-        </li>
-      </ol>
     </div>
   </div>
 @endsection
