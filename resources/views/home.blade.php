82165516bfc4a924cfebdc836bd2c6cc8eb27dba
hdmax25 <47875083+hdmax25@users.noreply.github.com>
2020-06-16 08:13:07 +0700

    Revert "Update home.blade.php"
    This reverts commit a442428854c7fa79a7da07f396e83bf59bb61d17.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -373,34 +373,6 @@
           </div>
         </div>
       </div>
-      <div class="col-md-12">
-        <div class="card card-danger collapsed-card">
-          <div class="card-header">
-            <h3 class="card-title">Keluhan</h3>
-            <div class="card-tools">
-              <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
-              </button>
-            </div>
-          </div>
-          <div class="card-body">
-            <table id="keluhan" class="table table-bordered table-striped" style="width: 100%">
-              <thead>
-              <tr>
-                <th>Keluhan</th>
-                <th>Jumlah</th>
-              </tr>
-              </thead>
-              <tbody>
-              @foreach($dataSakit as $item)
-                <tr>
-                  <td>{{ $id }}</td>
-                  <td>{{ $item }}</td>
-              @endforeach
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
     </div>
   @elseif (Auth::user()->role == 2)
     <div class="row">
