4c2d71115f17076d33d511c2b8b571d53a5416ba
hdmax25 <47875083+hdmax25@users.noreply.github.com>
2020-06-19 13:35:06 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -310,6 +310,8 @@
           <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
             </button>
+            <button type="button" class="btn btn-tool" data-card-widget="maximize"><i class="fas fa-expand"></i>
+            </button>
           </div>
         </div>
         <div class="card-body">
