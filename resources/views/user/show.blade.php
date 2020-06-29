d1333cc547bb69dc4aec145cafdb5b183486c739
hdmax25 <47875083+hdmax25@users.noreply.github.com>
2020-06-29 07:42:31 +0700

    Update show.blade.php

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -102,6 +102,7 @@
                 <div class="form-group row">
                   <label for="name" class="col-sm-2 col-form-label">Nama</label>
                   <div class="col-sm-10">
+                    <small>Awali dengan 62</small>
                     <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" placeholder="" value="{{ old('name') ? old('name') : $user->name }}">
                   </div>
                 </div>
