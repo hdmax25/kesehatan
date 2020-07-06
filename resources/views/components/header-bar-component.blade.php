c8e35aa117aef175d368edbed04efb1ee9310f3e
hdmax25 <47875083+hdmax25@users.noreply.github.com>
2020-07-06 09:20:39 +0700

    Update header-bar-component.blade.php

--- resources/views/components/header-bar-component.blade.php
+++ resources/views/components/header-bar-component.blade.php
@@ -4,12 +4,18 @@
             <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
         </li>
     </ul>
-
-    {{--  <ul class="navbar-nav ml-auto">--}}
-    {{--    <li class="nav-item">--}}
-    {{--      <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">--}}
-    {{--        <i class="fas fa-th-large"></i>--}}
-    {{--      </a>--}}
-    {{--    </li>--}}
-    {{--  </ul>--}}
+       
+        <!-- Right navbar links -->
+        <ul class="navbar-nav ml-auto">
+          <!-- Messages Dropdown Menu -->
+          <li class="nav-item dropdown">
+            <a class="nav-link" data-toggle="dropdown" href="#" aria-expanded="false">
+              <i class="far fa-comments"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right" style="left: inherit; right: 0px;">
+              <a href="https://wa.me/628980028222" class="dropdown-item dropdown-footer"><i class="fab fa-whatsapp mr-2"></i>Hubungi Admin</a>
+            </div>
+          </li>
+        </ul>
+      </nav>
 </nav>
