fdc3ef7f966ef6e3aa12a0802aefb6df4d819d32
hdmax25 <47875083+hdmax25@users.noreply.github.com>
2020-08-06 14:33:04 +0700

    Update side-bar-component.blade.php

--- resources/views/components/side-bar-component.blade.php
+++ resources/views/components/side-bar-component.blade.php
@@ -11,6 +11,7 @@
   <div class="sidebar">
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <div class="image">
+        {{-- <img src="{{ Auth::user()->image ? asset('dist/img/user/'. Auth::user()->image) : asset('dist/img/avatar5.png') }}" class="img-circle elevation-2" alt="User Image"> --}}
         <img src="{{ asset('dist/img/avatar5.png') }}" class="img-circle elevation-2" alt="User Image">
       </div>
       <div class="info">
