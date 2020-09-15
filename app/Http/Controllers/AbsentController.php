a45123db74e49428afd06f2432f5537cf328ec78
hdmax25 <47875083+hdmax25@users.noreply.github.com>
2020-09-15 07:06:20 +0700

    Update AbsentController.php

--- app/Http/Controllers/AbsentController.php
+++ app/Http/Controllers/AbsentController.php
@@ -178,6 +178,7 @@ class AbsentController extends Controller
       $data = [
         0 => [
           'EmpCode' => 'NIP',
+          'Name' => 'Nama',
           'Dt' => 'Tanggal',
           'Tm' => 'Jam',
           'City' => 'Log',
@@ -203,7 +204,7 @@ class AbsentController extends Controller
   
       foreach ($att as $id => $item) {
         $data[$id + 1]['EmpCode'] = $item->EmpCode;
-        $data[$id + 1]['Name'] = $item->user->username;
+        $data[$id + 1]['Name'] = $item->user->name;
         $data[$id + 1]['Dt'] = \Carbon\Carbon::parse($item->CreateDt)->format('d-m-Y');
         $data[$id + 1]['Tm'] = \Carbon\Carbon::parse($item->CreateDt)->format('H:i:s');
         $data[$id + 1]['City'] = $item->City;
