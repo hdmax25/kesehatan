f404c2de95c244c0f8eea8550c3a468876d8596a
hdmax25 <47875083+hdmax25@users.noreply.github.com>
2020-09-17 10:32:07 +0700

    Update report.blade.php

--- resources/views/absent/report.blade.php
+++ resources/views/absent/report.blade.php
@@ -15,10 +15,10 @@
     <div class="col-md-12">
       <div class="card card-danger card-outline">
         <div class="card-header">
-          <h3 class="card-title">Report</h3>
+          <h3 class="card-title">Absent</h3>
         </div>
-        <div class="card-body table-responsive p-0">
-          <table class="table table-hover table-sm table-nowarp">
+        <div class="card-body table-responsive table-sm">
+          <table id="report" class="table table-bordered table-striped table-sm">
             <thead>
               <tr>
                 <th>#</th>
@@ -41,9 +41,7 @@
             </tbody>
           </table>
         </div>
-        <!-- /.card-body -->
       </div>
-      <!-- /.card -->
     </div>
   </div>
   <div class="row">
@@ -77,17 +75,30 @@
 @endsection
 
 @section('css')
-  <!-- Toastr -->
-  <link rel="stylesheet" href="{{ asset('plugins/toastr/toastr.min.css') }}">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
 @endsection
 
 @section('js')
-  <!-- bs-custom-file-input -->
-  <script src="{{ asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}"></script>
-  <!-- Toastr -->
-  <script src="{{ asset('plugins/toastr/toastr.min.js') }}"></script>
+  <!-- DataTables -->
+  <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+  <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+  <script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+  <script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
 
-  <!-- IP Detect -->
-  <script src= "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> 
+  <script>
+    $(function () {
+      $('#report').DataTable({
+        "paging": true,
+        "lengthChange": true,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": true,
+        "responsive": true,
+      });
+    });
+  </script>
 
 @endsection
